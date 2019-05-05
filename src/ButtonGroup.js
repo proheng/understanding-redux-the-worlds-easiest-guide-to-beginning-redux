27f58887c11e819dcfb9a8cb6e51835781d93712
Rex Zhiheng He <rex@rex-he.net>
2019-05-05 21:55:06 +1000

    add dispatch action to onClick handler

--- src/ButtonGroup.js
+++ src/ButtonGroup.js
@@ -1,9 +1,28 @@
 import React from "react";
+import store from "./store";
+
+function setTechnology(text) {
+  return {
+    type: "SET_TECHNOLOGY",
+    text: text
+  };
+}
+
+function dispatchBtnAction(e) {
+  console.log(e.target.dataset.tech);
+  const tech = e.target.dataset.tech;
+  store.dispatch(setTechnology(tech));
+}
 
 const ButtonGroup = ({ technologies }) => (
   <div>
     {technologies.map((tech, i) => (
-      <button data-tech={tech} key={`btn-${i}`} className="hello-btn">
+      <button
+        data-tech={tech}
+        key={`btn-${i}`}
+        className="hello-btn"
+        onClick={dispatchBtnAction}
+      >
         {tech}
       </button>
     ))}
