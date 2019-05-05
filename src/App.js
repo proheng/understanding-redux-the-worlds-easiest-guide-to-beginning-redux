7688b4b88f86d137303980442cdf5c8f8adce42f
Rex Zhiheng He <rex@rex-he.net>
2019-05-05 21:48:38 +1000

    Create correct reducers, actions, and store folder. \n add ButtonGroup rendering function.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import HelloWorld from "./HelloWorld";
-import { createStore } from "redux";
-import reducer from "./reducers";
-
-const initialState = { tech: "React " };
-const store = createStore(reducer, initialState);
+import ButtonGroup from "./ButtonGroup";
+import store from "./store";
 
 class App extends Component {
   render() {
-    return <HelloWorld tech={store.getState().tech} />;
+    return [
+      <HelloWorld key={1} tech={store.getState().tech} />,
+      <ButtonGroup key={2} technologies={["React", "Elm", "React-redux"]} />
+    ];
   }
 }
 
