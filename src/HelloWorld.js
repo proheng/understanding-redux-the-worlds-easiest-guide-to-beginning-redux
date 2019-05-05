b75b3b7e5e26effaa9c3c1303c38e31484ec0129
Rex Zhiheng He <rex@rex-he.net>
2019-05-05 19:29:49 +1000

    create normal React code

---
+++ src/HelloWorld.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class HelloWorld extends Component {
+  render() {
+    return <h1>Hello World {this.props.tech} </h1>;
+  }
+}
+
+export default HelloWorld;
